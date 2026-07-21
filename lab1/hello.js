a0920ddf89313e8b4dbdb0038659c20a6c20f12d
Amandeep <amandeep09630@gmail.com>
2026-07-21 14:50:50 +0530

    hello

---
+++ lab1/hello.js
@@ -0,0 +1,16 @@
+function sayHello(name){
+    console.log(`welcome to ${name}`);
+}
+sayHello("Amandeep")
+
+function sum(a,b){
+    return a+b;
+}
+const findSum=(a,b)=>{
+    return a+b;
+}
+const findSum=(a,b)=>a+b;
+
+
+sayHello("amandeep");
+console.log(`sum of 2 and 5 is ${sum(2,5)}`)

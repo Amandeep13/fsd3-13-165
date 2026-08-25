9b88220f843f5793059ddca329afae0b3e0b2f07
Amandeep <amandeep09630@gmail.com>
2026-08-25 13:33:37 +0530

    cru

---
+++ lab2/crud.js
@@ -0,0 +1,91 @@
+import readline from "readline/promises";
+import { writeFile, readFile } from "fs/promises";
+import { stdin, stdout } from "process";
+
+const FILE = "./lab2/products.json";
+const savecart = async (cart) => {
+    await writeFile(FILE, JSON.stringify(cart, null, 2));
+
+};
+const getcart = async () => {
+    const data = await readFile(FILE, "utf-8");
+    return JSON.parse(data);
+
+}
+const addtocart = async (item) => {
+    const products = await getcart();
+    const productsFound = products.find((p) =>p.id ===item.id);
+    if(productFound){
+        product.qty +=item.qty;
+        console.log("product in cart quantity updated");
+    }else{
+        products.push(items);
+        console.log("Product added successfully 👍")
+    }
+    products.push(item);
+    await savecart(products);
+}
+const showcart = () => {
+    console.log("show cart");
+}
+const updatecart = () => {
+    console.log("cart updated");
+}
+const deletefromcart = () => {
+    console.log("deleted from cart");
+}
+
+const main = async () => {
+    const cin = readline.createInterface({ input: stdin, output: stdout });
+    let choice;
+    do {
+
+        console.log("Welcome to Shopping cart 🛍️")
+        console.log("1......Add to cart");
+        console.log("2......Show cart"); 5
+        console.log("3......Remove item");
+        console.log("4......Update Quantity");
+        console.log("5.......Checkout");
+        choice = await cin.question("Enter your choice : ");
+
+        switch (Number(choice)) {
+            case 1:
+                let data = await cin.question('Enter id , name ,price , qty :');
+                let p = data.split(",");
+                
+                let q=p.map((item)=>item.trim()) ;
+                let[id, name, price, qty] = q;
+                console.log(id, name, price, qty);
+                const product={
+                    id: Number(id) ,
+                    name,
+                    price: Number(price) ,
+                    qty: Number(qty) ,
+
+                } ;
+               // console.table(product) ;
+                await addtocart(product) ;
+                break;
+            case 2:
+                showcart();
+                break;
+            case 3:
+                deletefromcart();
+                break;
+            case 4:
+                updatecart();
+                break;
+            case 5:
+
+                break;
+            default:
+                console.log("Invalid choice! try again 💥")
+
+        }
+        console.log("type of", typeof (choice))
+    } while (choice !== "5");
+    cin.close();
+
+
+};
+await main();
--- lab2/readme.md
+++ lab2/readme.md
@@ -29,3 +29,18 @@ it directs connect with client OS rater than browser
     -File Operation
     
 
+##CRUD OPERATION 
+create/Insert,Read/Retrieve,Update,Delete
+##items
+each item (id,name,price,qty)
+##operations
+1.update quantiuty front cart
+2.Add to cart
+3.slow cart
+4.resume from cart
+5.checkout
+NOTe:all items will be stored in hdd,so after termination of program we can retrive cart details
+
+##Reqquired file
+1.crud.js -it contain all the methods and entry points
+2.products.json-it contains the product details in array form

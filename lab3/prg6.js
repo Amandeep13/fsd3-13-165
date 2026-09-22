3e0d4a75699adf743087d31582208ea275abf51b
Amandeep <amandeep09630@gmail.com>
2026-09-22 14:18:16 +0530

    products

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,11 +1,19 @@
 import http from 'http'
-
+import { getAllproducts } from './products.js';
 
 const server =http.createServer((req,res) => {
-      if (req.url === "/" && req.method === "GET") {
+      if (req.url === "/api/v1/products" && req.method === 'GET') {
         res.statusCode = 200;
-        res.end("Get Request");
-      } else if (req.url === "/" && req.method === "POST") {
+        const data =getAllProducts();
+        res.setHeader('content-type','application/json');
+        res.end(
+          JSOn.stringify ({
+            count:data.length,
+            data,
+          })
+
+        );
+      } else if (req.url === "/api/v1/products" && req.method === "POST") {
         // console.log("Request",req);
         let body=''
         req.on('data',(chunk)=>{
---
+++ lab3/products.js
@@ -0,0 +1,12 @@
+const products= [
+    { id:1,name:"sam",qty:100,price:1000  },
+    { id:2,name:"ram",qty:10,price:1000  },
+    
+]
+let nextId=3;
+  export const  getAllproducts = () =>{
+    return products;
+ }
+ export const addProduct = () =>{
+    
+ }
